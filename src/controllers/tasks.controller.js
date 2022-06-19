a4e4db557cca686a3660e4e204ace41f2ea0806f
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-06-19 16:12:37 -0500

    se crearon funciones en tasks.controller.js

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,3 +1,23 @@
- export const getTasks = (req, res) => {
+export const getTasks = (req, res) => {
+    res.send("sending tasks");
+}
+
+export const createTask = (req, res) => { //para crear tarea
+    res.send("sending tasks");
+}
+
+export const getTask = (req, res) => { //para obtener una unica tarea
+    res.send("sending tasks");
+}
+
+export const updateTask = (req, res) => { //para actualizar una tarea 
+    res.send("sending tasks");
+}
+
+export const deleteTask = (req, res) => { //para eliminar una tarea
+    res.send("sending tasks");
+}
+
+export const count = (req, res) => { //para contar tareas
     res.send("sending tasks");
 }
