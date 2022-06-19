027fc2080be96fa3a7b550dde1a8403da377cc39
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-06-19 16:42:35 -0500

    se modifico ultimo comentario de app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import taskRoutes from './routes/tasks.js';
 
 const app = express();
 
-app.use(express.json()) //cuando envian una dato en formato .json el podra leerlo y lo asignara en una variable llamada badi dentro de request  
+app.use(express.json()) //cuando envian una dato en formato .json el podra leerlo y lo asignara en una variable llamada body dentro de request  
 
 app.use(taskRoutes);
 
