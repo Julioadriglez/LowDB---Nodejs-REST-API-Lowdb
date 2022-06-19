75478f926e09ba3023f04c7d5456d603a2bf218b
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-06-19 16:31:10 -0500

    codigo para poder leer el formato json y mandarlo a quest

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import taskRoutes from './routes/tasks.js';
 
 const app = express();
 
-app.use(taskRoutes)
+app.use(express.json()) //cuando envian una dato en formato .json el podra leerlo y lo asignara en una variable llamada badi dentro de request  
+
+app.use(taskRoutes);
 
 export default app; //con esto esportamos app
