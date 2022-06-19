e9b2a8949a87bb960e0ee9d91328e125aa43f2b0
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-06-19 16:21:12 -0500

    Se importaron todos los metos de las direcciones a tasks.js

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { getTasks } from "../controllers/tasks.controller.js" //importamos el get tasks
+import { count, createTask, deleteTask, getTask, getTasks, updateTask } from "../controllers/tasks.controller.js" //importamos el get tasks
 const router = Router();
 
 router.get('/tasks', getTasks); //cuando visiten "/tasks" de ejecutara la función getTasks
-router.get('/tasks/count', getTasks); //para contar todas las tareas
-router.get('/tasks/:id', getTasks); //para obtener una unica tarea apartir del id que mande el usuario 
-router.post('/tasks', getTasks); //post para poder crear tareas 
-router.put('/tasks/:id', getTasks); //metodo put para actualizar 
-router.delete('/tasks/:id', getTasks);
+router.get('/tasks/count', count); //para contar todas las tareas
+router.get('/tasks/:id', getTask); //para obtener una unica tarea apartir del id que mande el usuario 
+router.post('/tasks', createTask); //post para poder crear tareas 
+router.put('/tasks/:id', updateTask); //metodo put para actualizar tarea
+router.delete('/tasks/:id', deleteTask); //Elinimar tarea
 export default router;
