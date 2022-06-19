6d204c8c33b2aebda515e671b0c0f6c589bb88a4
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-06-19 16:04:39 -0500

    se grearon las rutas get, post, put y delete

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import { getTasks } from "../controllers/tasks.controller.js" //importamos el get tasks
 const router = Router();
 
-router.get('/tasks', (req, res) => {
-    res.send("sending tasks");
-});
-
+router.get('/tasks', getTasks); //cuando visiten "/tasks" de ejecutara la función getTasks
+router.get('/tasks/count', getTasks); //para contar todas las tareas
+router.get('/tasks/:id', getTasks); //para obtener una unica tarea apartir del id que mande el usuario 
+router.post('/tasks', getTasks); //post para poder crear tareas 
+router.put('/tasks/:id', getTasks); //metodo put para actualizar 
+router.delete('/tasks/:id', getTasks);
 export default router;
